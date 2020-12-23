2f5ba709f3a3a9d23f202558aa901e4cd52a8d67
winter-sols <winter.solstice1219@gmail.com>
2020-12-22 22:43:17 -0800

    Remigrate the user

--- talent_hub/user/models.py
+++ talent_hub/user/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from . import constants
 
 class User(AbstractUser):
-
-    ROLE_TYPE = [
-        ('ROLE_ADMIN', 'admin'),
-        ('ROLE_TEAM_MANAGER', 'team manager'),
-        ('ROLE_DEVELOPER', 'developer')
-    ]
-
-    role = models.CharField(max_length=20, choices=ROLE_TYPE, default='ROLE_DEVELOPER')
-    
+    role = models.IntegerField(choices=constants.ROLE_TYPES, default=constants.ROLE_DEVELOPER)
