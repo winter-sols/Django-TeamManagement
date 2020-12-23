4e0fe611473012d38a2e709de1eededa1bc193a0
winter-sols <winter.solstice1219@gmail.com>
2020-12-22 22:42:21 -0800

    Put all constants in the User app to constants.py

---
+++ talent_hub/user/constants.py
@@ -0,0 +1,10 @@
+
+ROLE_ADMIN = 1
+ROLE_TEAM_MANAGER = 2
+ROLE_DEVELOPER = 3
+
+ROLE_TYPES = [
+    (ROLE_ADMIN, 'admin'),
+    (ROLE_TEAM_MANAGER, 'team manager'),
+    (ROLE_DEVELOPER, 'developer')
+]
