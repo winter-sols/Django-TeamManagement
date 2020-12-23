a32a518e379c33df9c2a11aa51b1a258218a2bcf
winter-sols <winter.solstice1219@gmail.com>
2020-12-23 08:05:27 -0800

    Set email field as USERNAME_FIELD

--- talent_hub/user/models.py
+++ talent_hub/user/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth.models import AbstractUser
 from . import constants
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     role = models.IntegerField(choices=constants.ROLE_TYPES, default=constants.ROLE_DEVELOPER)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+# class Profile(models.Model):
+#     type = 
