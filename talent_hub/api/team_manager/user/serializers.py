2c56d7d670df32e5a3950f54921a3bff87cfc5f4
winter-sols <winter.solstice1219@gmail.com>
2020-12-31 00:36:40 -0800

    Add id field to UserAdminSerializer and TeamUserListSerializer

--- talent_hub/api/team_manager/user/serializers.py
+++ talent_hub/api/team_manager/user/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class TeamUserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'username', 'first_name', 'last_name', 'role',)
+        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'role',)
