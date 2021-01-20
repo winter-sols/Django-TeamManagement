b180556e9845fec8da366afa01145925ec1264b9
winter-sols <winter.solstice1219@gmail.com>
2021-01-19 18:52:29 -0800

    Add id field to ClientSerializer

--- talent_hub/api/developer/finance/serializers.py
+++ talent_hub/api/developer/finance/serializers.py
@@ -4,5 +4,5 @@ from finance.models import Client
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ('type', 'full_name', 'company_name')
+        fields = ('id', 'type', 'full_name', 'company_name')
  
