10976890107c9db5ad9d4eb2543471805927561b
winter-sols <winter.solstice1219@gmail.com>
2021-01-19 17:50:36 -0800

    Add models in finance app to admin

--- talent_hub/finance/admin.py
+++ talent_hub/finance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client, Project, FinancialRequest, Partner, Transaction
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Project)
+admin.site.register(FinancialRequest)
+admin.site.register(Partner)
+admin.site.register(Transaction)
--- talent_hub/finance/models.py
+++ talent_hub/finance/models.py
@@ -8,16 +8,23 @@ class Client(models.Model):
     company_name = models.CharField(max_length=100, null=True)
     started_at = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return '{}({})'.format(self.full_name, self.type)
+
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
     type = models.IntegerField(choices=constants.PROJECT_TYPES)
     price = models.FloatField(null=True)
     client = models.ForeignKey('Client', on_delete=models.CASCADE)
-    participants = models.ManyToManyField('user.User', on_delete=models.CASCADE)
-    project_starter = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    participants = models.ManyToManyField('user.User', related_name='related_participants')
+    project_starter = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='related_project_starter')
     started_at = models.DateField(null=True)
     ended_at = models.DateField(null=True)
     status = models.IntegerField(choices=constants.PROJECT_STATUS)
+
+    def __str__(self):
+        return '{}({})'.format(self.title, self.type)
 
 
 class FinancialRequest(models.Model):
@@ -27,6 +34,9 @@ class FinancialRequest(models.Model):
     counter_party = models.CharField(max_length=50)
     requested_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return '{} {}'.format(self.type, self.status)
+
 
 class Transaction(models.Model):
     client = models.ForeignKey('Client', on_delete=models.CASCADE)
@@ -35,8 +45,12 @@ class Transaction(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     gross_amount = models.FloatField()
     net_amount = models.FloatField()
-    payment_platform = models.CharField(choices=constants.PAYMENT_PLATFORMS)
+    payment_platform = models.CharField(choices=constants.PAYMENT_PLATFORMS, max_length=10)
     related_financial = models.ForeignKey('FinancialRequest', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return '{} {} ({})'.format(self.client, self.project, self.payment_platform)
+
 
 class Partner(models.Model):
     full_name = models.CharField(max_length=50)
@@ -45,4 +59,7 @@ class Partner(models.Model):
     dob = models.DateField()
     phone_num = models.CharField(max_length=50, null=True)
     contact_method = JSONField()
+
+    def __str__(self):
+        return '{}({})'.format(self.full_name, self.email)
 
