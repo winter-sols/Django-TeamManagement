56422aa037a2b73f923df0899456b42814568e24
winter-sols <winter.solstice1219@gmail.com>
2021-01-19 18:03:43 -0800

    Define admin models in the finance app

--- talent_hub/finance/admin.py
+++ talent_hub/finance/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from .models import Client, Project, FinancialRequest, Partner, Transaction
 
-admin.site.register(Client)
-admin.site.register(Project)
-admin.site.register(FinancialRequest)
-admin.site.register(Partner)
-admin.site.register(Transaction)
+class ClientModelAdmin(admin.ModelAdmin):
+    list_display = ('type', 'full_name', 'company_name', 'started_at')
+
+
+class ProjectModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'client', 'price', 'started_at', 'ended_at', 'status')
+
+
+class FinancialRequestModelAdmin(admin.ModelAdmin):
+    list_display = ('type', 'status', 'amount', 'counter_party', 'requested_at')
+
+
+class TransactionModelAdmin(admin.ModelAdmin):
+    list_display = ('client', 'project', 'created_at', 'gross_amount', 'net_amount', 'payment_platform', 'related_financial')
+
+
+class PartnerModelAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email')
+
+
+admin.site.register(Client, ClientModelAdmin)
+admin.site.register(Project, ProjectModelAdmin)
+admin.site.register(FinancialRequest, FinancialRequestModelAdmin)
+admin.site.register(Partner, PartnerModelAdmin)
+admin.site.register(Transaction, TransactionModelAdmin)
