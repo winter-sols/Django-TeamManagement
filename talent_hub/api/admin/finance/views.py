9ca57ffc3b85012d9c177d92bc732a42f185a6d3
winter-sols <winter.solstice1219@gmail.com>
2021-01-28 13:17:57 -0800

    Implement api/admin/financial-requests/:id/approve/ end-points

---
+++ talent_hub/api/admin/finance/views.py
@@ -0,0 +1,16 @@
+from rest_framework.generics import UpdateAPIView
+from rest_framework.response import Response
+from api.common.finance.serializers import FinancialRequestDetailSerializer, FinancialRequestSerializer
+from finance.models import FinancialRequest
+from finance.constants import FINANCIAL_STATUS_APPROVED, FINANCIAL_STATUS_DECLINED
+
+class ApproveFinanicalRequestView(UpdateAPIView):
+    serializer_class = FinancialRequestDetailSerializer
+    queryset = FinancialRequest.objects.all()
+
+    def update(self, request, pk):
+        instance = self.get_object()
+        instance.status = FINANCIAL_STATUS_APPROVED
+        serializer = self.get_serializer(instance)
+        instance.save()
+        return Response(serializer.data)
--- talent_hub/api/admin/urls.py
+++ talent_hub/api/admin/urls.py
@@ -2,6 +2,7 @@ from django.urls import  include, path
 from rest_framework import routers
 from .user.views import UserAdminViewSet, TeamListViewSet, ProfileListAdminView, ProfilesAdminViewSet, AccountListByProfileIdView, AccountsAdminViewSet, TeamUserListView
 from api.common.finance.views import ClientViewSet, PartnerViewSet, ProjectViewSet, FinancialRequestViewSet
+from api.admin.finance.views import ApproveFinanicalRequestView
 
 router = routers.DefaultRouter()
 router.register('users', UserAdminViewSet)
@@ -19,5 +20,6 @@ router.register('financial-requests', FinancialRequestViewSet)
 urlpatterns = router.urls + [
     path('users/<int:pk>/profiles/', ProfileListAdminView.as_view(), name='user_profiles'),
     path('profiles/<int:pk>/accounts/', AccountListByProfileIdView.as_view(), name='profile_accounts'),
-    path('teams/<int:pk>/users/', TeamUserListView.as_view(), name='users_by_team_id')
+    path('teams/<int:pk>/users/', TeamUserListView.as_view(), name='users_by_team_id'),
+    path('financial-requests/<int:pk>/approve/', ApproveFinanicalRequestView.as_view(), name='approve_financial_requests')
 ]
