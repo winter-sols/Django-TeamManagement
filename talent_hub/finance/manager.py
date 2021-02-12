494a49998f174a003a78bcfb19ef9f5014783c9b
winter-sols <winter.solstice1219@gmail.com>
2021-02-12 06:28:16 -0800

    Implement reporting of approved requests

--- talent_hub/finance/manager.py
+++ talent_hub/finance/manager.py
@@ -2,7 +2,8 @@ from django.db.models import QuerySet
 from .constants import (
     PROJECT_STATUS_ONGOING,
     FINANCIAL_STATUS_PENDING,
-    FINANCIAL_TYPE_RCV_PAYMENT
+    FINANCIAL_STATUS_APPROVED,
+    FINANCIAL_TYPE_RCV_PAYMENT,
 )
 
 class ProjectManager(QuerySet):
@@ -13,3 +14,6 @@ class ProjectManager(QuerySet):
 class FinancialRequestMangager(QuerySet):
     def pending_requests(self):
         return self.filter(status=FINANCIAL_STATUS_PENDING)
+    
+    def approved_requests(self):
+        return self.filter(status=FINANCIAL_STATUS_APPROVED)
