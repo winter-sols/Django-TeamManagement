65a9ad0fd35063e07959ce182347e87c5e1792be
winter-sols <winter.solstice1219@gmail.com>
2021-02-12 08:24:52 -0800

    Block approving financial request if type is send invoice

--- talent_hub/api/admin/finance/views.py
+++ talent_hub/api/admin/finance/views.py
@@ -4,19 +4,24 @@ from rest_framework.response import Response
 from rest_framework import mixins
 from api.common.finance.serializers import FinancialRequestDetailSerializer, FinancialRequestSerializer, TransactionCreateSerializer
 from finance.models import FinancialRequest, Transaction
-from finance.constants import FINANCIAL_STATUS_APPROVED, FINANCIAL_STATUS_DECLINED
+from finance.constants import (
+    FINANCIAL_STATUS_APPROVED,
+    FINANCIAL_STATUS_DECLINED,
+    FINANCIAL_TYPE_SND_INVOICE
+)
 
 class ApproveFinanicalRequestView(UpdateAPIView):
     serializer_class = FinancialRequestDetailSerializer
     queryset = FinancialRequest.objects.all()
 
     def update(self, request, pk):
-        transaction_data = request.data
-        transaction_data['financial_request'] = pk
-        transaction_ser = TransactionCreateSerializer(data=transaction_data)
-        transaction_ser.is_valid(raise_exception=True)
-        transaction_ser.save()
-
+        financial_request = FinancialRequest.objects.get(id=pk)
+        if financial_request.type != FINANCIAL_TYPE_SND_INVOICE:
+            transaction_data = request.data
+            transaction_data['financial_request'] = pk
+            transaction_ser = TransactionCreateSerializer(data=transaction_data)
+            transaction_ser.is_valid(raise_exception=True)
+            transaction_ser.save()
         instance = self.get_object()
         instance.status = FINANCIAL_STATUS_APPROVED
         serializer = self.get_serializer(instance)
