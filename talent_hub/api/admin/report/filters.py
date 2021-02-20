0f62821171e620c912d7c8dde0a1eb88a660f404
winter-sols <winter.solstice1219@gmail.com>
2021-02-20 12:05:04 -0800

    Implement report filtering

--- talent_hub/api/admin/report/filters.py
+++ talent_hub/api/admin/report/filters.py
@@ -1,6 +1,6 @@
 from rest_framework import filters
 from django_filters import FilterSet
-from user.models import User
+from user.models import User, Team
 
 class DeveloperReportFilter(FilterSet):
     """
@@ -9,3 +9,12 @@ class DeveloperReportFilter(FilterSet):
     class Meta:
         model = User
         fields = ['id', 'team']
+
+
+class TeamReportFilter(FilterSet):
+    """
+    filter financial reports by team id and name
+    """
+    class Meta:
+        model = Team
+        fields = ['id', 'name']
