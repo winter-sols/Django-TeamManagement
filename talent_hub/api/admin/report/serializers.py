9743ecae328afaf87544d21e2f7e13358f88c944
winter-sols <winter.solstice1219@gmail.com>
2021-02-19 17:07:20 -0800

    Implement financial report of team

--- talent_hub/api/admin/report/serializers.py
+++ talent_hub/api/admin/report/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from user.models import User
+from user.models import User, Team
 
 class DeveloperReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name')
 
+
+class TeamReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ('id', 'name')
--- talent_hub/api/admin/report/views.py
+++ talent_hub/api/admin/report/views.py
@@ -1,3 +1,4 @@
+import numpy as np
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
@@ -7,8 +8,11 @@ from api.utils.provider import (
     get_this_quarter_earning,
     get_this_week_earning
 )
-from user.models import User
-from .serializers import DeveloperReportSerializer
+from user.models import User, Team
+from .serializers import (
+    DeveloperReportSerializer, 
+    TeamReportSerializer
+)
 from .filters import DeveloperReportFilter
 
 class DeveloperMonthlyReportView(APIView):
@@ -25,6 +29,26 @@ class DeveloperMonthlyReportView(APIView):
             reports[index] = DeveloperReportSerializer(queryset[index]).data
             reports[index]['earning'] = get_this_month_earning(queryset[index])
 
+        return Response(reports)
+
+
+class TeamMonthlyReportView(APIView):
+    permission_classes = [IsAdmin]
+
+    def get(self, request):
+        queryset = Team.objects.all()
+        count = queryset.count()
+        reports = list(range(count))
+
+        for index in range(count):
+            reports[index] = TeamReportSerializer(queryset[index]).data
+            member_set = queryset[index].user_set.all()
+            member_cnt = member_set.count()
+            team_earnings = list(range(member_cnt))
+            for member_index in range(member_cnt):
+                team_earnings[member_index] = get_this_month_earning(member_set[member_index])
+            reports[index]['earning'] = np.sum(team_earnings)
+        
         return Response(reports)
 
 
@@ -45,6 +69,26 @@ class DeveloperQuarterlyReportView(APIView):
         return Response(reports)
 
 
+class TeamQuarterlyReportView(APIView):
+    permission_classes = [IsAdmin]
+
+    def get(self, request):
+        queryset = Team.objects.all()
+        count = queryset.count()
+        reports = list(range(count))
+
+        for index in range(count):
+            reports[index] = TeamReportSerializer(queryset[index]).data
+            member_set = queryset[index].user_set.all()
+            member_cnt = member_set.count()
+            team_earnings = list(range(member_cnt))
+            for member_index in range(member_cnt):
+                team_earnings[member_index] = get_this_quarter_earning(member_set[member_index])
+            reports[index]['earning'] = np.sum(team_earnings)
+        
+        return Response(reports)
+
+
 class DeveloperWeeklyReportView(APIView):
     permission_classes = [IsAdmin]
     filter_backends = (DjangoFilterBackend,)
@@ -59,4 +103,24 @@ class DeveloperWeeklyReportView(APIView):
             reports[index] = DeveloperReportSerializer(queryset[index]).data
             reports[index]['earning'] = get_this_week_earning(queryset[index])
 
+        return Response(reports)
+
+
+class TeamWeeklyReportView(APIView):
+    permission_classes = [IsAdmin]
+
+    def get(self, request):
+        queryset = Team.objects.all()
+        count = queryset.count()
+        reports = list(range(count))
+
+        for index in range(count):
+            reports[index] = TeamReportSerializer(queryset[index]).data
+            member_set = queryset[index].user_set.all()
+            member_cnt = member_set.count()
+            team_earnings = list(range(member_cnt))
+            for member_index in range(member_cnt):
+                team_earnings[member_index] = get_this_week_earning(member_set[member_index])
+            reports[index]['earning'] = np.sum(team_earnings)
+        
         return Response(reports)
