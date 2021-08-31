c935d7ece138c90bb32cd0e86607888ad79b4e54
winter-sols <winter.solstice1219@gmail.com>
2021-08-31 11:09:56 -0700

    Write custom object manager for Notification model

--- notification/models.py
+++ notification/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+import datetime
+
+class NotificationManager(models.Manager):
+    def unread_items(self, owner):
+        return super().get_queryset().filter(notify_to=owner, read_at__isnull=True)
+    def read_items(self, owner):
+        return super().get_queryset().filter(notify_to=owner, read_at__isnull=False)
 
 
 class Notification(models.Model):
-  notify_to = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='Receivers')
-  creator = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='Creators')
-  message = models.TextField('Notification message', null=True)
-  content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-  object_id = models.PositiveIntegerField()
-  content_object = GenericForeignKey('content_type','object_id')
-  read = models.BooleanField(default=False)
+    notify_to = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='received_notifications')
+    creator = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='created_notifications')
+    message = models.TextField('Notification message', null=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type','object_id')
+    read_at = models.DateTimeField(null=True, blank=True)
 
-  def __str__(self):
-    return '{} ,created by {}'.format(self.content_object, self.creator)
+    objects = NotificationManager()
+    
+    def __str__(self):
+        return '{} created by {}'.format(self.message, self.creator)
+
+    def mark_as_read(self):
+        self.read_at = datetime.datetime.now()
+        self.save()
