c5024c1e691fa84f0aaeaeac7da3dec8824810f7
winter-sols <winter.solstice1219@gmail.com>
2021-09-16 00:40:19 -0700

    Fix Api endpoint for my team reports

--- api/team_manager/report/views.py
+++ api/team_manager/report/views.py
@@ -1,7 +1,7 @@
 import numpy as np
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, GenericAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from api.permission import IsTeamManager
 from api.utils.provider import (
@@ -10,71 +10,66 @@ from api.utils.provider import (
 )
 from user.models import User, Team
 from api.common.report.serializers import (
-    TeamMonthlyReportSerializer,
-    TeamQuarterlyReportSerializer,
-    TeamWeeklyReportSerializer,
+    DeveloperMonthlyReportSerializer,
+    DeveloperQuarterlyReportSerializer,
+    DeveloperWeeklyReportSerializer,
 )
-from api.common.report.filters import TeamReportFilter
-
+from api.common.report.filters import DeveloperReportFilter, DeveloperReportFilter
 
 class TeamMonthlyReportView(ListAPIView):
     permission_classes = [IsTeamManager]
-    serializer_class = TeamMonthlyReportSerializer
     filter_backends = (DjangoFilterBackend,)
-    filterset_class = TeamReportFilter
+    filterset_class = DeveloperReportFilter
+    serializer_class = DeveloperMonthlyReportSerializer
 
     def get_queryset(self):
-        return Team.objects.filter(id=self.request.user.team.id)
+        return User.objects.filter(team=self.request.user.team.id)
 
 
 class TeamQuarterlyReportView(ListAPIView):
     permission_classes = [IsTeamManager]
-    serializer_class = TeamQuarterlyReportSerializer
+    serializer_class = DeveloperQuarterlyReportSerializer
     filter_backends = (DjangoFilterBackend,)
-    filterset_class = TeamReportFilter
+    filterset_class = DeveloperReportFilter
 
     def get_queryset(self):
-        return Team.objects.filter(id=self.request.user.team.id)
+        return User.objects.filter(team=self.request.user.team.id)
 
 
 class TeamWeeklyReportView(ListAPIView):
     permission_classes = [IsTeamManager]
-    serializer_class = TeamWeeklyReportSerializer
+    serializer_class = DeveloperWeeklyReportSerializer
     filter_backends = (DjangoFilterBackend,)
-    filterset_class = TeamReportFilter
+    filterset_class = DeveloperReportFilter
 
     def get_queryset(self):
-        return Team.objects.filter(id=self.request.user.team.id)
+        return User.objects.filter(team=self.request.user.team.id)
 
 
-class TeamCustomReportView(APIView):
+class TeamCustomReportView(GenericAPIView):
     permission_classes = [IsTeamManager]
 
+    def get_queryset(self):
+        return User.objects.filter(team=self.request.user.team.id)
+        
     def get(self, request):
         start_date = self.request.query_params.get('from')
         end_date = self.request.query_params.get('to')
-        queryset = Team.objects.filter(id=self.request.user.team.id)
-        team_cnt = queryset.count()
-        response = list(range(team_cnt))
+        queryset = self.filter_queryset(self.get_queryset())
+        user_cnt = queryset.count()
+        response = list(range(user_cnt))
+        page = self.paginate_queryset(queryset)
 
-        for idx in range(team_cnt):
-            team = queryset[idx]
-            userset = team.user_set.all()
-            user_cnt = userset.count()
-            sub_res = list(range(user_cnt))
-            total = 0
-            for usr_idx in range(user_cnt):
-                user = userset[usr_idx]
-                earning = get_custom_earning(user, user.role, start_date, end_date)
-                total += earning
-                sub_res[usr_idx] = {
-                    'id': user.id,
-                    'full_name': user.first_name + ' ' + user.last_name,
-                    'earning': earning
-                }
-            
+        for idx in range(user_cnt):
+            user = queryset[idx]
             response[idx] = {
-                'team_earnings': sub_res,
-                'total': total
+                'id': user.id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'earning': get_custom_earning(user, start_date, end_date),
+                'project_earnings': get_custom_project_earning(user, start_date, end_date)
             }
+
+        if page is not None:
+            return self.get_paginated_response(response)
         return Response(response)
