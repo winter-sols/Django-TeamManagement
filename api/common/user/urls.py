6a55d6f57fdf5e4ce25d9201be1773e99cd2f48d
winter-sols <winter.solstice1219@gmail.com>
2021-10-09 11:12:34 -0400

    Created API endpoints related to users in team-manger role

--- api/common/user/urls.py
+++ api/common/user/urls.py
@@ -1,11 +1,11 @@
 from rest_framework import routers
 from django.urls import path
 
-from .views import UserAdminViewSet, ProfileListAdminView
+from .views import UserViewSet, ProfileListAdminView
 
 
 router = routers.DefaultRouter()
-router.register('', UserAdminViewSet)
+router.register('', UserViewSet, basename="users")
 
 urlpatterns = router.urls + [
     path('<int:pk>/profiles/', ProfileListAdminView.as_view(), name='user_profiles'),
