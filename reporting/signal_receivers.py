473a276aaa7b090efc4c81d941c72b351bbdb670
winter-sols <winter.solstice1219@gmail.com>
2022-01-27 22:05:54 +0900

    Prevent creating duplicate logging report notifications

--- reporting/signal_receivers.py
+++ reporting/signal_receivers.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import datetime
+
 from .models import Log
 from user.models import User
 from notification.models import Notification
@@ -7,9 +9,9 @@ from notification.models import Notification
 
 @receiver(post_save, sender=Log)
 def create_notification_whenever_logged(sender, instance, created, **kwargs):
-    print(User.objects.filter_admins())
     if instance.owner.is_team_manager:
         for admin in User.objects.filter_admins():
+            Notification.objects.unread_items(admin).filter(creator=instance.owner).update(read_at=datetime.datetime.now())
             Notification.objects.create(
                 notify_to=admin,
                 creator=instance.owner,
@@ -17,6 +19,7 @@ def create_notification_whenever_logged(sender, instance, created, **kwargs):
                 content_object=instance
             )
     if instance.owner.is_developer and instance.owner.team_manager is not None:
+        Notification.objects.unread_items(instance.owner.team_manager).filter(creator=instance.owner).update(read_at=datetime.datetime.now())
         Notification.objects.create(
             notify_to=instance.owner.team_manager,
             creator=instance.owner,
