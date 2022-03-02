ff8f4739e57e3020187013d20393cce6c224d4ae
winter-sols <winter.solstice1219@gmail.com>
2022-03-02 18:25:44 +0900

    change full_name to first_name and last_name

--- api/search/user/serializers.py
+++ api/search/user/serializers.py
@@ -10,14 +10,9 @@ from finance.models import (
 )
 
 class SearchUserSerializer(serializers.ModelSerializer):
-    full_name = serializers.SerializerMethodField()
-
-    def get_full_name(self, obj):
-        return obj.first_name + ' ' + obj.last_name
-    
     class Meta:
         model = User
-        fields = ('id', 'full_name')
+        fields = ('id', 'first_name', 'last_name')
 
 
 class SearchProfileSerializer(serializers.ModelSerializer):
